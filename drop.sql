be385e15a18bed55dfc628f3daafc7455319d9a4
Simone Pierazzini <makeroo@gmail.com>
2014-03-09 21:07:57 +0100

    Completato script drop

---
+++ drop.sql
@@ -0,0 +1,14 @@
+DROP TABLE transaction_line;
+DROP TABLE transaction;
+DROP TABLE permission_grant;
+DROP TABLE permission;
+DROP TABLE csa;
+DROP TABLE account_person;
+DROP TABLE person_contact;
+DROP TABLE person;
+DROP TABLE account;
+DROP TABLE delivery_place;
+DROP TABLE contact_address;
+DROP TABLE street_address;
+DROP TABLE city;
+DROP TABLE state;
