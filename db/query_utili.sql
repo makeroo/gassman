91bb124f8c4e46343da114ed29474e90cd83e03a
Simone Pierazzini <makeroo@gmail.com>
2014-03-16 14:41:44 +0100

    Aggiunte queri per gestire il merge di due profili.

--- db/query_utili.sql
+++ db/query_utili.sql
@@ -32,7 +32,31 @@ select * from account
  where id not in (select current_account_id from person where current_account_id is not null)
    and gc_parent = 'acf998ffe1edbcd44bc30850813650ac';
 
-   
+-- profili doppi
+-- uso: si trovano due profili (person) che riferiscono la stessa persona fisica
+--      (ovviamente in maniera non automatica, tipo persone che si regisrano più
+--      volte con account openid diversi)
+-- si trovano i person_contact interessati
+select p.id as pid, p.first_name, p.last_name,
+       c.id as cid, c.address, c.kind, c.contact_type,
+       pc.id as xid
+  from person_contact pc
+  join person p on pc.person_id=p.id
+  join contact_address c on c.id=pc.address_id
+  where p.id in (40,36);
+-- quindi si assegnano i person_contact del person che si vuol buttare
+-- al person che si vuol tenere
+update person_contact set person_id=36 where id in (79,80);
+-- NB: una cosa del genere, cioè un passo solo, non si può fare:
+-- update person_contact set person_id=36 where id in (select pc.id from person_contact pc join person p on pc.person_id=p.id join contact_address c on c.id=pc.address_id where p.id =40);
+-- ERROR 1093 (HY000): You can't specify target table 'person_contact' for update in FROM clause
+
+-- verificare se un profilo (person) ha dati associati
+select * from person_contact where person_id=40;
+select * from account_person where person_id=40;
+select * from permission_grant where person_id=40;
+
+
 select * from account where gc_name like '%igong%';
 update person set current_account_id =83 where id=15;
 
