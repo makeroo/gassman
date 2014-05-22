25eb32aa1260c1a4956aaf4af691382dc0648da6
Simone Pierazzini <makeroo@gmail.com>
2014-05-22 09:21:34 +0200

    Dimenticato di includerlo...

---
+++ db/gas_init.sql
@@ -0,0 +1,8 @@
+INSERT INTO permission (id, name, visibility) VALUES (1, 'membership', 10);
+INSERT INTO permission (id, name, visibility) VALUES (2, 'canCheckAccounts', 1000);
+INSERT INTO permission (id, name, visibility) VALUES (3, 'canAssignAccounts', 10000);
+INSERT INTO permission (id, name, visibility) VALUES (4, 'canEnterDeposit', 2000);
+INSERT INTO permission (id, name, visibility) VALUES (5, 'canEnterPayments', 2000);
+INSERT INTO permission (id, name, visibility) VALUES (6, 'canManageTransactions', 5000);
+INSERT INTO permission (id, name, visibility) VALUES (7, 'canEnterCashExchange', 1000);
+INSERT INTO permission (id, name, visibility) VALUES (8, 'canEnterWithdrawal', 2000);
