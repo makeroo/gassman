39fb6a2f05ed9dcb89ff017b74fdff79c4183184
Simone Pierazzini <makeroo@gmail.com>
2014-06-17 06:47:47 +0200

    Rimuovo i vecchi aggiornamenti di db, non servono a nessuno.

--- db/update_schema_006_007.sql
+++
@@ -1,7 +0,0 @@
-SET SESSION storage_engine = "MyISAM";
-
-alter table account drop column gc_desc;
-alter table account drop column gc_id;
-alter table account drop column gc_parent;
-alter table transaction drop column gc_id;
-alter table transaction_line drop column gc_id;
