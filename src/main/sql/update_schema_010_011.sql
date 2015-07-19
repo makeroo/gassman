288cb74b2ea4d078fdb18c833b1e60f94090c3ef
Simone Pierazzini <makeroo@gmail.com>
2015-07-19 06:58:03 +0200

    Corretto script di aggiornamento db

--- src/main/sql/update_schema_010_011.sql
+++ src/main/sql/update_schema_010_011.sql
@@ -1,6 +1,6 @@
-alter table delivery_place add column csa_id int not null ;
+SET SESSION storage_engine = "MyISAM";
 
-update state set name='Italia' where id=110;
+alter table delivery_place add column csa_id int not null ;
 
 insert into street_address (first_line, city_id) values ('Sant''Anna', 3680);
 insert into street_address (first_line, city_id) values ('via di Tempagnano', 3680);
@@ -11,8 +11,6 @@ insert into delivery_place (address_id, description, csa_id) values (2, 'Tempagn
 insert into delivery_place (address_id, description, csa_id) values (3, 'Pieve San Paolo', 1);
 
 
--- creo il produttore irene e luciana (1)
-insert into producer (name, description) values ('Luciana & Irene', 'Erbi, uova, polli, conigli etc.');
--- aggiungo simone pieraz come referente (può gestirne gli ordini)
-insert into producer_person (producer_id, person_id, perm_id, csa_id) values (1, 1, 12, 1);
+update state set name='Italia' where id=110;
+
 
