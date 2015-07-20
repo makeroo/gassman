db8e009c5723971e788f283cf503def5eb99d2c1
Simone Pierazzini <makeroo@gmail.com>
2015-07-20 08:34:07 +0200

    Rimossi gli update, li lascio solo nei branch prd...

--- src/main/sql/update_schema_008_009.sql
+++
@@ -1,3 +0,0 @@
-SET SESSION storage_engine = "MyISAM";
-
-ALTER TABLE city ADD COLUMN state VARCHAR(10);
--- src/main/sql/update_schema_009_010.sql
+++
@@ -1,6 +0,0 @@
-SET SESSION storage_engine = "MyISAM";
-
-ALTER TABLE account ADD COLUMN membership_fee DECIMAL(15,2) NOT NULL DEFAULT 0;
-ALTER TABLE csa DROP COLUMN annual_kitty_amount;
-
-INSERT INTO permission (id, name, visibility) VALUES (12, 'canEditMembershipFee', 1000);
