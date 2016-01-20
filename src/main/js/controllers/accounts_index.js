aeae05795dfb080ed108566bb5a58da2b034ae80
Simone Pierazzini <makeroo@gmail.com>
2016-01-20 06:41:08 +0100

    pulizia

--- src/main/js/controllers/accounts_index.js
+++ src/main/js/controllers/accounts_index.js
@@ -6,90 +6,12 @@
 
 angular.module('GassmanApp.controllers.AccountsIndex', [
     'GassmanApp.services.Gdata',
+    'GassmanApp.services.listController',
     'ngStorage',
     'GassmanApp.filters.HumanTimeDiff',
     'GassmanApp.filters.AlphaTimeDiff'
 ])
 
-.service('listController',
-[
-function () {
-	this.setupScope = function ($scope, dataService, options) {
-        options = options || {};
-
-        if (options.storage)
-            $scope.pagination = options.storage[options.storageKey];
-
-        if (!$scope.pagination)
-            $scope.pagination = {
-                totalItems: 0,
-                page: options.firstPage || 1,
-                pageSize: (+options.pageSize || 15).toString(),
-                filterBy: options.filterBy,
-                pageSizes: options.pageSizes || [ 15, 30, 60, 100 ]
-            };
-
-		$scope.items = null;
-		$scope.initError = null;
-		$scope.pageError = null;
-
-		$scope.$watch('pagination.page', function (n, o) {
-			if ($scope.pagination.page > 0 && o != n) {
-				$scope.loadPage($scope.pagination.page);
-			}
-		});
-
-		$scope.$watch('pagination.pageSize', function (n, o) {
-			if (n > 0 && n != o) {
-				$scope.pageCount = null;
-				$scope.loadPage(1);
-			}
-		});
-
-		$scope.$watch('pagination.filterBy', function (n, o) {
-            //questo serve a evitare il doppio ricaricamento della pagina in caso di filtro e pagina corrente != dalla prima
-            if (n == o) {
-                return;
-            } else if ($scope.pagination.page != 1) {
-                $scope.pagination.page = 1;
-            } else {
-                $scope.loadPage(1);
-            }
-		}, true);
-
-		$scope.loadPage = function (p) {
-            if (options.storage) {
-                options.storage[options.storageKey] = angular.copy($scope.pagination);
-            }
-
-			$scope.pagination.page = p;
-
-            var sz = +$scope.pagination.pageSize;
-			var from = (p - 1) * sz;
-
-			dataService(from, sz, $scope.pagination.filterBy).
-			then (function (r) {
-                $scope.pagination.totalItems = r.data.count;
-				$scope.items = r.data.items;
-
-				if (options) {
-                    try {
-                        options.pageLoadedHook();
-                    } catch (e) {
-                        console.log('pageLoadedHook failed', e);
-                    }
-				}
-			}).
-			then (undefined, function (error) {
-				$scope.pageError = error.data;
-			});
-		};
-
-        if (options.loadFirstPage !== false) {
-            $scope.loadPage($scope.pagination.page);
-        }
-	};
-}])
 
 .controller('AccountsIndex', [
          '$scope', '$state', '$location', 'gdata', '$localStorage', 'listController',
@@ -184,70 +106,4 @@ function ($scope,   $state,   $location,   gdata,   $localStorage,   listControl
         $scope.deliveryPlaces = null;
     });
 }])
-/*
-.controller('AccountsIndex', [
-         '$scope', '$filter', '$localStorage', '$q', 'gdata',
-function ($scope,   $filter,   $location,   ,   $q,   gdata) {
-    $scope.accounts = [];
-    $scope.accountsError = null;
-//	$scope.queryFilter = $localStorage.accountIndex_queryFilter || '';
-//	$scope.queryOrder = 0;
-
-    var start = 0;
-    var blockSize = 25;
-    $scope.concluded = false;
-
-//	var lastQuery = ;
-//	var lastQueryOrder = 0;
-//	var lastDeliveryPlace = -1;
-
-    $scope.query = angular.copy(lastQuery);
-
-    $scope.search = function () {
-        if (angular.equals($scope.query, lastQuery))
-            return;
-
-        lastQuery = angular.copy($scope.query);
-
-        $localStorage.accountIndex_queryFilter = $scope.query.q;
-
-        reset();
-        $scope.loadMore();
-    };
-
-    var reset = function () {
-        $scope.accounts = [];
-        $scope.accountsError = null;
-        start = 0;
-        $scope.concluded = false;
-    };
-
-    var loading = false;
-
-    $scope.$watch('query.dp', $scope.search);
-
-    $scope.loadMore = function () {
-        if ($scope.concluded || loading) return;
-
-        loading = true;
-
-        gdata.accountsIndex(
-            $scope.gassman.selectedCsa,
-            lastQuery,
-            start, blockSize).
-        then(function (r) {
-            loading = false;
-            $scope.concluded = r.data.length < blockSize;
-            start += r.data.length;
-            $scope.accounts = $scope.accounts.concat(r.data);
-
-        }).
-        then (undefined, function (error) {
-            loading = false;
-            $scope.concluded = true;
-            $scope.accountsError = error.data;
-        });
-    };
-}])
-*/
 ;
---
+++ src/main/js/services/list_controller.js
@@ -0,0 +1,90 @@
+/**
+ * Created by makeroo on 27/04/15.
+ */
+
+'use strict';
+
+angular.module('GassmanApp.services.listController', [
+])
+
+
+.service('listController',
+[
+function () {
+	this.setupScope = function ($scope, dataService, options) {
+        options = options || {};
+
+        if (options.storage)
+            $scope.pagination = options.storage[options.storageKey];
+
+        if (!$scope.pagination)
+            $scope.pagination = {
+                totalItems: 0,
+                page: options.firstPage || 1,
+                pageSize: (+options.pageSize || 15).toString(),
+                filterBy: options.filterBy,
+                pageSizes: options.pageSizes || [ 15, 30, 60, 100 ]
+            };
+
+		$scope.items = null;
+		$scope.initError = null;
+		$scope.pageError = null;
+
+		$scope.$watch('pagination.page', function (n, o) {
+			if ($scope.pagination.page > 0 && o != n) {
+				$scope.loadPage($scope.pagination.page);
+			}
+		});
+
+		$scope.$watch('pagination.pageSize', function (n, o) {
+			if (n > 0 && n != o) {
+				$scope.pageCount = null;
+				$scope.loadPage(1);
+			}
+		});
+
+		$scope.$watch('pagination.filterBy', function (n, o) {
+            //questo serve a evitare il doppio ricaricamento della pagina in caso di filtro e pagina corrente != dalla prima
+            if (n == o) {
+                return;
+            } else if ($scope.pagination.page != 1) {
+                $scope.pagination.page = 1;
+            } else {
+                $scope.loadPage(1);
+            }
+		}, true);
+
+		$scope.loadPage = function (p) {
+            if (options.storage) {
+                options.storage[options.storageKey] = angular.copy($scope.pagination);
+            }
+
+			$scope.pagination.page = p;
+
+            var sz = +$scope.pagination.pageSize;
+			var from = (p - 1) * sz;
+
+			dataService(from, sz, $scope.pagination.filterBy).
+			then (function (r) {
+                $scope.pagination.totalItems = r.data.count;
+				$scope.items = r.data.items;
+
+				if (options) {
+                    try {
+                        options.pageLoadedHook();
+                    } catch (e) {
+                        console.log('pageLoadedHook failed', e);
+                    }
+				}
+			}).
+			then (undefined, function (error) {
+				$scope.pageError = error.data;
+			});
+		};
+
+        if (options.loadFirstPage !== false) {
+            $scope.loadPage($scope.pagination.page);
+        }
+	};
+}])
+;
