48b317a1f179c21ea64cfb861fe3c2c1cff4538b
Simone Pierazzini <s.pierazzini@nextworks.it>
2017-03-24 05:43:06 +0100

    Fixed createdb tool

--- tools/createdb.sh
+++ tools/createdb.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sed -e "s/--.*//g" ../gas.ddl | mysql -u gassman -p gassman
+COMMAND="`which $0`"
+COMMAND_HOME="`dirname "$COMMAND"`"
+
+sed -e "s/--.*//g" $COMMAND_HOME/src/main/sql/gas.ddl | mysql -u gassman -p gassman
